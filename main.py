72c721dcced2e1808a9c6f76141c31d589d2b2d9
rajveersinghcse <rajveersinghcse@gmail.com>
2024-05-05 10:26:39 +0530

    updated

--- main.py
+++ main.py
@@ -11,20 +11,25 @@ from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     # Read Video
-    video_frames = read_video('./input_videos/match.mp4')
+    video_frames = read_video("./input_videos/match.mp4")
 
     # Initialize Tracker
-    tracker = Tracker('./models/best.pt')
+    tracker = Tracker("./models/best.pt")
 
-    tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
-    # Get object positions 
+    tracks = tracker.get_object_tracks(
+        video_frames, read_from_stub=True, stub_path="stubs/track_stubs.pkl"
+    )
+    # Get object positions
     tracker.add_position_to_tracks(tracks)
 
     # camera movement estimator
     camera_movement_estimator = CameraMovementEstimator(video_frames[0])
-    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,read_from_stub=True,stub_path='stubs/camera_movement_stub.pkl')
-    camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
-
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(
+        video_frames, read_from_stub=True, stub_path="stubs/camera_movement_stub.pkl"
+    )
+    camera_movement_estimator.add_adjust_positions_to_tracks(
+        tracks, camera_movement_per_frame
+    )
 
     # View Trasnformer
     view_transformer = ViewTransformer()
@@ -39,43 +44,51 @@ def main():
 
     # Assign Player Teams
     team_assigner = TeamAssigner()
-    team_assigner.assign_team_color(video_frames[0], 
-                                    tracks['players'][0])
-    
-    for frame_num, player_track in enumerate(tracks['players']):
-        for player_id, track in player_track.items():
-            team = team_assigner.get_player_team(video_frames[frame_num],track['bbox'], player_id)
-            tracks['players'][frame_num][player_id]['team'] = team 
-            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+    team_assigner.assign_team_color(video_frames[0], tracks["players"][0])
 
-    
+    for frame_num, player_track in enumerate(tracks["players"]):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(
+                video_frames[frame_num], track["bbox"], player_id
+            )
+            tracks["players"][frame_num][player_id]["team"] = team
+            tracks["players"][frame_num][player_id]["team_color"] = (
+                team_assigner.team_colors[team]
+            )
+
     # Assign Ball Aquisition
-    player_assigner =PlayerBallAssigner()
-    team_ball_control= []
-    for frame_num, player_track in enumerate(tracks['players']):
-        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+    player_assigner = PlayerBallAssigner()
+    team_ball_control = []
+    for frame_num, player_track in enumerate(tracks["players"]):
+        ball_bbox = tracks["ball"][frame_num][1]["bbox"]
         assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
 
         if assigned_player != -1:
-            tracks['players'][frame_num][assigned_player]['has_ball'] = True
-            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+            tracks["players"][frame_num][assigned_player]["has_ball"] = True
+            team_ball_control.append(
+                tracks["players"][frame_num][assigned_player]["team"]
+            )
         else:
             team_ball_control.append(team_ball_control[-1])
-    team_ball_control= np.array(team_ball_control)
+    team_ball_control = np.array(team_ball_control)
 
-
-    # Draw output 
+    # Draw output
     ## Draw object Tracks
-    output_video_frames = tracker.draw_annotations(video_frames, tracks,team_ball_control)
+    output_video_frames = tracker.draw_annotations(
+        video_frames, tracks, team_ball_control
+    )
 
     ## Draw Camera movement
-    output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
+    output_video_frames = camera_movement_estimator.draw_camera_movement(
+        output_video_frames, camera_movement_per_frame
+    )
 
     ## Draw Speed and Distance
-    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames,tracks)
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
 
     # Save video
-    save_video(output_video_frames, './output_videos/output_video.avi')
+    save_video(output_video_frames, "./output_videos/output_video.avi")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
